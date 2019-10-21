d33572ef2e240103e0f073f4114177380c1a971b
Mario Ferreira <mariosilvaosbourne@gmail.com>
2019-10-21 01:29:04 -0300

    Ajustes de CSS e estrutura - Componentes e criação do carousel - Nosso APP

--- src/components/painel/index.js
+++ src/components/painel/index.js
@@ -3,10 +3,14 @@ import React, { Fragment } from 'react';
 import Home from '../../assets/home.png';
 import '../painel/styles.css';
 
+var styleBannerPrincipal = {
+  backgroundImage: `url(${Home})`
+};
+
 export default function Painel() {
   return (
     <Fragment>
-      <div className="banner-principal">
+      <div className="banner-principal" style={styleBannerPrincipal}>
       </div>
     </Fragment>
   );
