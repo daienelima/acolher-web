53a487f5358f6161f3c105bc4e716473490ad830
daienelima <daiene.m17@gmail.com>
2019-10-27 15:25:40 -0300

    inclusao de api no web

--- src/components/conectados/index.js
+++ src/components/conectados/index.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import '../conectados/styles.css';
 import Consultas from '../../assets/Consulta.png';
 import Pessoas from '../../assets/heart.png';
 import Instituicao from '../../assets/Instituicao.png';
+import api from '../../services/api';
 
 var stylePessoas = {
   backgroundImage: `url(${Pessoas})`
@@ -16,31 +17,49 @@ var styleConsultas = {
 var styleInstituicao = {
   backgroundImage: `url(${Instituicao})`
 };
+export default class Conectados extends Component {
+  state = {
+    qtdPessoasAjudadas: 0,
+    qtdConsultasRealizadas: 0,
+    qtdInstituicoes: 0
+  };
 
-export default function Conectados() {
-  return (
-    <Fragment>
-      <div className="container-conectados">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm item">
-              <div className="icon" style={stylePessoas}></div>
-              <h3>1000</h3>
-              <h5>Pessoas ajudadas</h5>
-            </div>
-            <div className="col-sm item">
-              <div className="icon" style={styleConsultas}></div>
-              <h3>1200</h3>
-              <h5>Consultas agendadas</h5>
-            </div>
-            <div className="col-sm item">
-              <div className="icon" style={styleInstituicao}></div>
-              <h3>300</h3>
-              <h5>Instituições associadas</h5>
+  componentDidMount() {
+    api.get('usuario').then(res => {
+      this.setState({ qtdPessoasAjudadas: res.data.length });
+    });
+    api.get('instituicao').then(res => {
+      this.setState({ qtdInstituicoes: res.data.length });
+    });
+    api.get('consulta').then(res => {
+      this.setState({ qtdConsultasRealizadas: res.data.length });
+    });
+  }
+  render() {
+    return (
+      <Fragment>
+        <div className="container-conectados">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm item">
+                <div className="icon" style={stylePessoas}></div>
+                <h3>{this.state.qtdPessoasAjudadas}</h3>
+                <h5>Pessoas ajudadas</h5>
+              </div>
+              <div className="col-sm item">
+                <div className="icon" style={styleConsultas}></div>
+                <h3>{this.state.qtdConsultasRealizadas}</h3>
+                <h5>Consultas agendadas</h5>
+              </div>
+              <div className="col-sm item">
+                <div className="icon" style={styleInstituicao}></div>
+                <h3>{this.state.qtdInstituicoes}</h3>
+                <h5>Instituições associadas</h5>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Fragment>
-  );
+      </Fragment>
+    );
+  }
 }
--- src/components/nos/index.js
+++ src/components/nos/index.js
@@ -8,10 +8,11 @@ export default function Nos() {
       <div className="container-nos container">
         <strong>Sobre</strong>
         <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-         when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap 
-         into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-         passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          O Acolher é um projeto que utiliza a tecnologia como aliada no combate
+          ao transtorno depressivo. Nascido em 2019 com a missão de facilitar o
+          acesso ao tratamento, estudantes de tecnologia do Recife/PE dicidiram
+          criar uma aplicação para que as pessoas que precisam de ajuda tenham
+          facilidade no acesso ao tratamento.
         </p>
       </div>
     </Fragment>
