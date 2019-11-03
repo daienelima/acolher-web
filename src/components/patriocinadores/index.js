36fb73cc75d2d2f7c5495da71670d83c74112a65
daienelima <daiene.m17@gmail.com>
2019-11-02 21:35:29 -0300

    inclusao de patriocinadores

--- src/components/patriocinadores/index.js
+++ src/components/patriocinadores/index.js
@@ -1,12 +1,19 @@
 import React, { Fragment } from 'react';
 
 import '../patriocinadores/styles.css';
+import Patriocinador01 from '../../assets/01.png';
+import Patriocinador02 from '../../assets/02.png';
+import Patriocinador03 from '../../assets/03.png';
+import Patriocinador04 from '../../assets/04.png';
 
 export default function Patriocinadores() {
   return (
     <Fragment>
-      <div>
-        <label>Patriocinadores...</label>
+      <div className="espaco">
+        <img src={Patriocinador01} alt="" />
+        <img src={Patriocinador03} alt="" />
+        <img src={Patriocinador04} alt="" />
+        <img src={Patriocinador02} alt="" />
       </div>
     </Fragment>
   );
